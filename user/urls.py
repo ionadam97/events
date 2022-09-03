8cf2051f1c0d076bc22da7cc703a43befe9113bc
ionadam97 <100874180+ionadam97@users.noreply.github.com>
2022-09-03 09:55:27 +0300

    send mail registration (#1)

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('register/', views.registerUser, name='register'),
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+    path('activate/<uidb64>/<token>/',views.activate, name='activate'),
 
 
     path('profiles', views.profiles, name='profiles'),
@@ -12,6 +13,7 @@ urlpatterns = [
     path('account/', views.userAccount, name='account'),
 
     path('edith_account/', views.edithAccount, name='edith_account'),
+
 
 
 ]
--- user/views.py
+++ user/views.py
@@ -6,6 +6,20 @@ from django.contrib.auth import login, authenticate, logout
 from .forms import RegisterForm, ProfileForm
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from .tokens import account_activation_token
+
+UserModel = get_user_model() 
+
+
 def registerUser(request):
     form = RegisterForm()
 
@@ -14,12 +28,46 @@ def registerUser(request):
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
+            user.is_active = False
             user.save()
-            login(request, user)
-            return redirect('login')
+
+            # to get the domain of the current site  
+            current_site = get_current_site(request)  
+            mail_subject = 'Activation link has been sent to your email id'  
+            message = render_to_string('acc_active_email.html', {  
+                'user': user,  
+                'domain': current_site.domain,  
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
+                'token':account_activation_token.make_token(user),  
+            })  
+            to_email = form.cleaned_data.get('email')  
+            email = EmailMessage(  
+                        mail_subject, message, to=[to_email]  
+            )  
+            email.send()  
+            return HttpResponse('Please confirm your email address to complete the registration')
+
+            # login(request, user)
+            # return redirect('login')
 
     context= {'form':form}
     return render(request, 'user/register.html', context)
+
+
+def activate(request, uidb64, token):  
+    User = get_user_model()  
+    try:  
+        uid = force_str(urlsafe_base64_decode(uidb64))  
+        user = User.objects.get(pk=uid)  
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):  
+        user = None  
+    if user is not None and account_activation_token.check_token(user, token):  
+        user.is_active = True  
+        user.save()  
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')  
+    else:  
+        return HttpResponse('Activation link is invalid!')  
+
 
 def loginUser(request):
     if request.user.is_authenticated:
