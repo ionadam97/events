3037ac3e041fde5f892716b989c3e28d184e588b
ionadam97 <adamion03@gmail.com>
2022-09-07 13:14:36 +0300

    data

--- events/settings.py
+++ events/settings.py
@@ -89,24 +89,24 @@ WSGI_APPLICATION = 'events.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'events',
-#         'USER': 'postgres',
-#         'PASSWORD': 'initiez1',
-#         'HOST': 'localhost',
-#         'PORT': '5544',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'events',
+        'USER': 'postgres',
+        'PASSWORD': 'initiez1',
+        'HOST': 'localhost',
+        'PORT': '5544',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
